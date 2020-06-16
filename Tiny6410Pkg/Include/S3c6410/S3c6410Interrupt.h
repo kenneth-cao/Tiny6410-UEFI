dc1f7dac16ddec1148c6ffc3f6e648ee2ce28d25
Kenneth <caochaochao@126.com>
2020-06-16 22:33:18 +0800

    Del extra macros

--- Tiny6410Pkg/Include/S3c6410/S3c6410Interrupt.h
+++ Tiny6410Pkg/Include/S3c6410/S3c6410Interrupt.h
@@ -15,15 +15,8 @@
 #ifndef __S3C6410INTERRUPT_H__
 #define __S3C6410INTERRUPT_H__
 
-// Interrupt
 #define VIC0_BASE           0x71200000
 #define VIC1_BASE           0x71300000
-
-#define INTMOD              0x00C    // VIC INT SELECT (IRQ or FIQ)
-#define INTUNMSK            0x010    // VIC INT EN (Unmask by writing 1)
-#define INTMSK              0x014    // VIC INT EN CLEAR (Mask by writing 1)
-#define INTSUBMSK           0x01C    // VIC SOFT INT CLEAR
-#define VECTADDR            0xF00    // VIC ADDRESS
 
 #define VICxIRQSTATUS       0x000
 #define VICxFIQSTATUS       0x004
@@ -41,36 +34,7 @@
 #define VICxVECTADDR(x)     (0x100 + 4 * (x))
 #define VICxVECPRIORITY(x)  (0x200 + 4 * (x))
 
-#define INT_NROF_VECTORS    64
-#define MAX_VECTOR          (INT_NROF_VECTORS - 1)
-
-
-#define INTERRUPT_BASE (0x48200000)
-
-#define INTCPS_SYSCONFIG      (INTERRUPT_BASE + 0x0010)
-#define INTCPS_SYSSTATUS      (INTERRUPT_BASE + 0x0014)
-#define INTCPS_SIR_IRQ        (INTERRUPT_BASE + 0x0040)
-#define INTCPS_SIR_IFQ        (INTERRUPT_BASE + 0x0044)
-#define INTCPS_CONTROL        (INTERRUPT_BASE + 0x0048)
-#define INTCPS_PROTECTION     (INTERRUPT_BASE + 0x004C)
-#define INTCPS_IDLE           (INTERRUPT_BASE + 0x0050)
-#define INTCPS_IRQ_PRIORITY   (INTERRUPT_BASE + 0x0060)
-#define INTCPS_FIQ_PRIORITY   (INTERRUPT_BASE + 0x0064)
-#define INTCPS_THRESHOLD      (INTERRUPT_BASE + 0x0068)
-#define INTCPS_ITR(n)         (INTERRUPT_BASE + 0x0080 + (0x20 * (n)))
-#define INTCPS_MIR(n)         (INTERRUPT_BASE + 0x0084 + (0x20 * (n)))
-#define INTCPS_MIR_CLEAR(n)   (INTERRUPT_BASE + 0x0088 + (0x20 * (n)))
-#define INTCPS_MIR_SET(n)     (INTERRUPT_BASE + 0x008C + (0x20 * (n)))
-#define INTCPS_ISR_SET(n)     (INTERRUPT_BASE + 0x0090 + (0x20 * (n)))
-#define INTCPS_ISR_CLEAR(n)   (INTERRUPT_BASE + 0x0094 + (0x20 * (n)))
-#define INTCPS_PENDING_IRQ(n) (INTERRUPT_BASE + 0x0098 + (0x20 * (n)))
-#define INTCPS_PENDING_FIQ(n) (INTERRUPT_BASE + 0x009C + (0x20 * (n)))
-#define INTCPS_ILR(m)         (INTERRUPT_BASE + 0x0100 + (0x04 * (m)))
-
-#define INTCPS_ILR_FIQ            BIT0
-#define INTCPS_SIR_IRQ_MASK       (0x7F)
-#define INTCPS_CONTROL_NEWIRQAGR  BIT0
-#define INTCPS_CONTROL_NEWFIQAGR  BIT1
+#define MAX_VECTOR          63
 
 #endif // __S3C6410INTERRUPT_H__
 
--- Tiny6410Pkg/Include/S3c6410/S3c6410Timer.h
+++ Tiny6410Pkg/Include/S3c6410/S3c6410Timer.h
@@ -20,77 +20,17 @@
 #define TIMER_CON          0x7F006008
 #define TIMER_INT_CSTAT    0x7F006044
 
-#define TIMER0_BASE      0x7F00600C
-#define TIMER1_BASE      0x7F006018
-#define TIMER2_BASE      0x7F006024
-#define TIMER3_BASE      0x7F006030
-#define TIMER4_BASE      0x7F00603c
+#define TIMER0_BASE        0x7F00600C
+#define TIMER1_BASE        0x7F006018
+#define TIMER2_BASE        0x7F006024
+#define TIMER3_BASE        0x7F006030
+#define TIMER4_BASE        0x7F00603c
 
-#define TIMER_CNTB     0x0
-#define TIMER_COMPB    0x4
-#define TIMER_CONTO    0x8
+#define TIMER_CNTB         0x0
+#define TIMER_COMPB        0x4
+#define TIMER_CONTO        0x8
 
-#define TCFG0_PRESCALER        4
-#define TCFG1_DIVIDER_1        0
-
-#define GPTIMER5_BASE   (0x49038000)
-#define GPTIMER6_BASE   (0x4903A000)
-#define GPTIMER7_BASE   (0x4903C000)
-#define GPTIMER8_BASE   (0x4903E000)
-#define GPTIMER9_BASE   (0x49040000)
-#define GPTIMER10_BASE  (0x48086000)
-#define GPTIMER11_BASE  (0x48088000)
-#define GPTIMER12_BASE  (0x48304000)
-#define WDTIMER2_BASE   (0x48314000)
-
-#define GPTIMER_TIOCP_CFG (0x0010)
-#define GPTIMER_TISTAT    (0x0014)
-#define GPTIMER_TISR      (0x0018)
-#define GPTIMER_TIER      (0x001C)
-#define GPTIMER_TWER      (0x0020)
-#define GPTIMER_TCLR      (0x0024)
-#define GPTIMER_TCRR      (0x0028)
-#define GPTIMER_TLDR      (0x002C)
-#define GPTIMER_TTGR      (0x0030)
-#define GPTIMER_TWPS      (0x0034)
-#define GPTIMER_TMAR      (0x0038)
-#define GPTIMER_TCAR1     (0x003C)
-#define GPTIMER_TSICR     (0x0040)
-#define GPTIMER_TCAR2     (0x0044)
-#define GPTIMER_TPIR      (0x0048)
-#define GPTIMER_TNIR      (0x004C)
-#define GPTIMER_TCVR      (0x0050)
-#define GPTIMER_TOCR      (0x0054)
-#define GPTIMER_TOWR      (0x0058)
-
-#define WSPR              (0x048)
-
-#define TISR_TCAR_IT_FLAG_MASK  BIT2
-#define TISR_OVF_IT_FLAG_MASK   BIT1
-#define TISR_MAT_IT_FLAG_MASK   BIT0
-#define TISR_ALL_INTERRUPT_MASK (TISR_TCAR_IT_FLAG_MASK | TISR_OVF_IT_FLAG_MASK | TISR_MAT_IT_FLAG_MASK)
-
-#define TISR_TCAR_IT_FLAG_NOT_PENDING   (0UL << 2)
-#define TISR_OVF_IT_FLAG_NOT_PENDING    (0UL << 1)
-#define TISR_MAT_IT_FLAG_NOT_PENDING    (0UL << 0)
-#define TISR_NO_INTERRUPTS_PENDING      (TISR_TCAR_IT_FLAG_NOT_PENDING | TISR_OVF_IT_FLAG_NOT_PENDING | TISR_MAT_IT_FLAG_NOT_PENDING)
-
-#define TISR_TCAR_IT_FLAG_CLEAR BIT2
-#define TISR_OVF_IT_FLAG_CLEAR  BIT1
-#define TISR_MAT_IT_FLAG_CLEAR  BIT0
-#define TISR_CLEAR_ALL          (TISR_TCAR_IT_FLAG_CLEAR | TISR_OVF_IT_FLAG_CLEAR | TISR_MAT_IT_FLAG_CLEAR)
-
-#define TCLR_AR_AUTORELOAD      BIT1
-#define TCLR_AR_ONESHOT         (0UL << 1)
-#define TCLR_ST_ON              BIT0
-#define TCLR_ST_OFF             (0UL << 0)
-
-#define TIER_TCAR_IT_ENABLE     (BIT2
-#define TIER_TCAR_IT_DISABLE    (0UL << 2)
-#define TIER_OVF_IT_ENABLE      BIT1
-#define TIER_OVF_IT_DISABLE     (0UL << 1)
-#define TIER_MAT_IT_ENABLE      BIT0
-#define TIER_MAT_IT_DISABLE     (0UL << 0)
+#define TCFG0_PRESCALER    4
 
 #endif // __S3C6410TIMER_H__
 
